247335ffc3ce28960ff58392ffbd309794652fbf
vishwassinfinity <vishwassinfinity@gmail.com>
2025-09-30 20:53:27 +0530

    Initial Commit

---
+++ WhisperZen/WhisperZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperZenApp.swift
+//  WhisperZen
+//
+//  Created by Vishwas B S on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WhisperZenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhisperZenTests/WhisperZenTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperZenTests.swift
+//  WhisperZenTests
+//
+//  Created by Vishwas B S on 30/09/25.
+//
+
+import Testing
+@testable import WhisperZen
+
+struct WhisperZenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
